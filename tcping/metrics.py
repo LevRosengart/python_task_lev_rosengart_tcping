fa5f51dd044d64bc2ce7834b33249ef8d059fe22
LevRosengart <levkonst2007@gmail.com>
2026-08-18 13:10:43 +0500

    add metrics.py

---
+++ tcping/metrics.py
@@ -0,0 +1,37 @@
+from tcping.ping_data import PingData
+
+
+class PingMetrics:
+    def __init__(self) -> None:
+        self._max_rtt: float | None = None
+        self._min_rtt: float | None = None
+        self._sum_rtt: float  = 0.0
+        self._sent_packets_count: int = 0
+        self._received_packets_count: int = 0
+
+    def record(self, ping_info: PingData) -> None:
+        self._sent_packets_count += 1
+        if ping_info.success:
+            self._received_packets_count += 1
+            self._max_rtt = max(self._max_rtt, ping_info.rtt) if self._max_rtt is not None else ping_info.rtt
+            self._min_rtt = min(self._min_rtt, ping_info.rtt) if self._min_rtt is not None else ping_info.rtt
+
+    @property
+    def max_rtt(self) -> float | None:
+        return self._max_rtt
+
+    @property
+    def min_rtt(self) -> float | None:
+        return self._min_rtt
+
+    @property
+    def avg_rtt(self) -> float | None:
+        return self._sum_rtt / self._sent_packets_count if self._sent_packets_count > 0 else None
+
+    @property
+    def sent_packets_count(self) -> int:
+        return self._sent_packets_count
+
+    @property
+    def received_packets_count(self) -> int:
+        return self._received_packets_count
--- tcping/time_metrics.py
+++
@@ -1,5 +0,0 @@
-class TimeMetrics:
-    def __init__(self):
-        self._max_time: float | None = None
-        self._min_time: float | None = None
-        self._avg_time: float | None = None
