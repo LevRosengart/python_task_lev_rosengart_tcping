e2e0315c109178f116c2120a100433125850be06
LevRosengart <levkonst2007@gmail.com>
2026-09-02 12:18:50 +0500

    add stop by ctrl+C

--- tcping/__main__.py
+++ tcping/__main__.py
@@ -38,21 +38,24 @@ def main() -> None:
     print(
         f"Pinging {args.host}:{args.port} with {args.pings_count} pings, timeout {args.timeout} sec, interval {args.interval} sec"
     )
-    for ping_count in range(1, args.pings_count + 1):
-        try:
-            ping_info: PingData = tcp_pinger.ping(mss=1452, sack_permitted=True)
-            metrics.record(ping_info)
-        except Exception as e:  # NOQA
-            print("Unexpected error")
-        else:
-            if ping_info.success:
-                print(f"PING {ping_count}\tRound-Trip Time: {ping_info.rtt:.5f} sec")
+    try:
+        for ping_count in range(1, args.pings_count + 1):
+            try:
+                ping_info: PingData = tcp_pinger.ping(mss=1452, sack_permitted=True)
+                metrics.record(ping_info)
+            except Exception as e:  # NOQA
+                print("Unexpected error")
             else:
-                print(f"PING {ping_count}\tPacket is loss")
-        if ping_count < args.pings_count:
-            time.sleep(args.interval)
-
-    print(StatsFormatter.format(metrics))
+                if ping_info.success:
+                    print(f"PING {ping_count}\tRound-Trip Time: {ping_info.rtt:.5f} sec")
+                else:
+                    print(f"PING {ping_count}\tPacket is loss")
+            if ping_count < args.pings_count:
+                time.sleep(args.interval)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        print(StatsFormatter.format(metrics))
 
 
 if __name__ == "__main__":
